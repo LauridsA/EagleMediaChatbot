91506a3cc7efd753bed43a23a7c0635ec04c9821
LauridsA <lauridsandersen2013@gmail.com>
2018-05-16 13:43:25 +0200

    seems to work

--- routes/botman.php
+++ routes/botman.php
@@ -7,7 +7,5 @@ $botman = resolve('botman');
 $botman->hears('Hej', function ($bot) {
     $bot->reply('Hej');
 });
-$botman->hears('YOUR_PAYLOAD_TEXT', function ($bot) {
-    $bot->reply('payload recieved');
-});
+$botman->hears('YOUR_PAYLOAD_TEXT', BotManController::class . '@startConversation');
 $botman->hears('Start', BotManController::class . '@startConversation');
