5cfe4e56cdde25bd4552e6174e0b36c7e53f4ec6
LauridsA <lauridsandersen2013@gmail.com>
2018-05-16 13:02:39 +0200

    fixing getting started button

--- routes/botman.php
+++ routes/botman.php
@@ -7,7 +7,5 @@ $botman = resolve('botman');
 $botman->hears('Hej', function ($bot) {
     $bot->reply('Hej');
 });
-$botman->hears('GET_STARTED_NEW_USER', function ($bot) {
-    $bot->reply('what');
-});
+$botman->hears('YOUR_PAYLOAD_TEXT', BotManController::class . '@startConversation');
 $botman->hears('Start', BotManController::class . '@startConversation');
