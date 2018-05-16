901cb2b862ff2bb200efbf6268dacf1ec84c454d
LauridsA <lauridsandersen2013@gmail.com>
2018-05-16 09:04:22 +0200

    Trying to obtain user information from botman.php

--- routes/botman.php
+++ routes/botman.php
@@ -4,13 +4,10 @@ use App\Http\Controllers\BotManController;
 $botman = resolve('botman');
 
 $botman->hears('Hej', function ($bot) {
-    $uname =  $bot->getUser()->getUsername();
-    $fname =  $bot->getUser()->getFirstName();
-    file_put_contents('PHP_ERROR_LOG.txt', $uname, FILE_APPEND);
-    file_put_contents('PHP_ERROR_LOG.txt', $fname, FILE_APPEND);
-    foreach ($bot->getUser()->getInfo() as $info){
-        file_put_contents('PHP_ERROR_LOG.txt', $info . '\n', FILE_APPEND);
-    }
+    $user = $bot->getUser();
+    $name = $user->getFirstName();
+    $bot->reply($name.'');
+    //$email = $user->getEmail();
     $bot->reply('Hej');
 });
 $botman->hears('Start', BotManController::class.'@startConversation');
